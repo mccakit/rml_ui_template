d83b488a2773dd9f45932e91ae8fa8f7b8ad94ac
mccakit <117523238+mccakit@users.noreply.github.com>
2025-06-08 01:26:47 +0300

    auto-copy assets

--- build/extern/sdl_image/external/libtiff-build/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/cakit/Desktop/RmlUi-hi/extern/sdl_image/external/libtiff/build
-# Build directory: C:/Users/cakit/Desktop/RmlUi-hi/build/extern/sdl_image/external/libtiff-build/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/extern/sdl_image/external/libtiff-build/port/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/cakit/Desktop/RmlUi-hi/extern/sdl_image/external/libtiff/port
-# Build directory: C:/Users/cakit/Desktop/RmlUi-hi/build/extern/sdl_image/external/libtiff-build/port
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
